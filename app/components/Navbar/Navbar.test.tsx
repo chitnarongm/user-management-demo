f66321cbc68b32b63fb655ec2a8178e9a0c9bfea
Chitnarong Munkong <chitnarong.mun@gmail.com>
2024-07-31 15:26:49 +0700

    add navbar click signout test

--- app/components/Navbar/Navbar.test.tsx
+++ app/components/Navbar/Navbar.test.tsx
@@ -1,11 +1,14 @@
 import { render, screen } from "@testing-library/react";
-import * as nextAuth from "next-auth/react";
+import userEvent from "@testing-library/user-event";
+import { signOut, useSession } from "next-auth/react";
 import Navbar from ".";
+
+jest.mock("next-auth/react");
 
 describe("Navbar", () => {
   beforeEach(() => {
     jest
-      .spyOn(nextAuth, "useSession")
+      .mocked(useSession)
       .mockReturnValue({ data: { user: { email: "mockuser@email.com" } }, status: "authenticated" } as any);
   });
   const renderNavbar = () => {
@@ -27,7 +30,7 @@ describe("Navbar", () => {
   });
 
   it("should not display user info section and logout button when user session is not existed", () => {
-    jest.spyOn(nextAuth, "useSession").mockReturnValue({ data: null, status: "unauthenticated" } as any);
+    jest.mocked(useSession).mockReturnValue({ data: null, status: "unauthenticated" } as any);
 
     renderNavbar();
 
@@ -35,5 +38,13 @@ describe("Navbar", () => {
     expect(screen.getByTestId("navbar-title")).toHaveTextContent("User Management");
     expect(screen.queryByTestId("user-info")).not.toBeInTheDocument();
     expect(screen.queryByTestId("logout-button")).not.toBeInTheDocument();
+  });
+
+  it("should call signOut function when click logout button ", async () => {
+    renderNavbar();
+
+    await userEvent.click(screen.getByTestId("logout-button"));
+
+    expect(signOut).toHaveBeenCalledWith({ callbackUrl: "/login" });
   });
 });
